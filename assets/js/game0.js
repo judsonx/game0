64db6733e582779c14a3c1020f2592c6f38e001c
Judson Weissert <judson.weissert@gmail.com>
2015-12-14 01:00:05 -0500

    Significant refactoring.
    Refactor for separation of concerns between game0.html and game0.js.
    Also, fix level 0 time interval bug.

--- assets/js/game0.js
+++ assets/js/game0.js
@@ -1,6 +1,6 @@
 var game0 = (function ($, document) {
-  var COLS = 10;
-  var ROWS = 20;
+  var COLS;
+  var ROWS;
 
   var CELL_EMPTY = 0;
   var CELL_S0 = 1;
@@ -9,8 +9,6 @@ var game0 = (function ($, document) {
   var CELL_S3 = 4;
   var CELL_S4 = 5;
 
-  var game_area_e_;
-  var preview_area_e_;
   var state_;
   var pos_ = { x: -1, y: -1 };
   var next_shape_ = 0;
@@ -18,7 +16,6 @@ var game0 = (function ($, document) {
   var orientation_ = 0;
   var score_ = 0;
   var lines_ = 0;
-  var paused_ = true;
 
   var shape0_ = {
     index: CELL_S0,
@@ -200,48 +197,6 @@ var game0 = (function ($, document) {
     pos_ = { x: Math.floor (COLS / 2), y: -1 * offset };
   }
 
-  function build_grid (element, N_ROWS, N_COLS)
-  {
-    for (var i = 0; i < N_ROWS; ++i)
-    {
-      var row = $(document.createElement ('div')).prop ({class: 'row'});
-      for (var j = 0; j < N_COLS; ++j)
-      {
-        $(document.createElement ('div')).appendTo (row);
-      }
-      row.appendTo (element);
-    }
-  }
-
-  function init_ga (game_area_e)
-  {
-    build_grid (game_area_e, ROWS, COLS);
-    game_area_e_ = game_area_e;
-  }
-
-  function init_pa (preview_area_e)
-  {
-    var PREVIEW_SIZE = 4;
-    build_grid (preview_area_e, PREVIEW_SIZE, PREVIEW_SIZE);
-    preview_area_e_ = preview_area_e;
-  }
-
-  function update_preview (shape_index)
-  {
-    var shape = shapes_[shape_index][0];
-    preview_area_e_.children ().each (function (row_index, row_div) {
-      $(row_div).children ().each (function (col_index, cell_div) {
-        var value = CELL_EMPTY;
-        if (row_index < shape.size.y && col_index <= shape.size.x)
-        {
-          if (shape.grid[row_index][col_index])
-            value = shape.index;
-        }
-        update_cell ($(cell_div), value);
-      })
-    });
-  }
-
   function reset ()
   {
     state_ = create_empty_state ();
@@ -250,42 +205,18 @@ var game0 = (function ($, document) {
     orientation_ = 0;
     score_ = 0;
     lines_ = 0;
-    update_preview (next_shape_);
+    update_preview_cb (shapes_[next_shape_][0]);
     reset_pos ();
-    update_game (state_, state_);
+    update_game_cb (state_, state_);
+    return { score: score_, lines: lines_ };
   }
 
   function init (config)
   {
-    init_ga (config.game_area);
-    init_pa (config.preview_area);
-  }
-
-  function update_cell (element, id)
-  {
-    element.removeClass ('s0 s1 s2 s3 s4');
-    var map = [
-      function (e) {},
-      function (e) { e.addClass ('s0'); },
-      function (e) { e.addClass ('s1'); },
-      function (e) { e.addClass ('s2'); },
-      function (e) { e.addClass ('s3'); },
-      function (e) { e.addClass ('s4'); },
-    ];
-
-    map[id] (element);
-  }
-
-  function update_game (state0, state1)
-  {
-    game_area_e_.children ().each (function (row_index, row_div) {
-       $(row_div).children ().each (function (col_index, cell_div) {
-         if (state0[row_index][col_index])
-           update_cell ($(cell_div), state0[row_index][col_index]);
-         else
-           update_cell ($(cell_div), state1[row_index][col_index]);
-       })
-    });
+    update_preview_cb = config.preview_cb;
+    update_game_cb = config.game_cb;
+    ROWS = config.n_rows;
+    COLS = config.n_cols;
   }
 
   function place_shape (state0, state, shape, pos)
@@ -324,24 +255,28 @@ var game0 = (function ($, document) {
     if (place_shape (state_, state, shape, next_pos))
     {
       pos_ = next_pos;
-      update_game (state_, state);
-      return true;
+      update_game_cb (state_, state);
+
+      return { score: score_, lines: lines_, done: false };
     }
 
     if (!place_shape (state_, state_, shape, pos_))
-      return false;
+    {
+      return { score: score_, lines: lines_, done: true };
+    }
 
     orientation_ = 0;
     current_shape_ = next_shape_;
     next_shape_ = get_next_shape ();
-    update_preview (next_shape_);
+    var shape = shapes_[next_shape_][0];
+    update_preview_cb (shape);
     reset_pos ();
     var result = clear_full_rows (state_);
     state_ = result.state;
     score_ += Math.pow (result.count, 2);
     lines_ += result.count;
 
-    return true;
+    return { score: score_, lines: lines_, done: false };
   }
 
   function get_next_orientation (shape_index, orientation)
@@ -361,49 +296,41 @@ var game0 = (function ($, document) {
     var shape = shapes_[current_shape_][try_orientation];
     if (place_shape (state_, state, shape, pos_))
     {
-      update_game (state_, state);
+      update_game_cb (state_, state);
       orientation_ = try_orientation;
     }
   }
 
   function on_ctrl_left ()
   {
-    if (paused_)
-      return;
-
     var state = create_empty_state ();
     var try_pos = { x: pos_.x - 1, y: pos_.y };
     if (place_shape (state_, state, current_shape (), try_pos))
     {
-      update_game (state_, state);
+      update_game_cb (state_, state);
       pos_.x -= 1;
     }
   }
 
   function on_ctrl_right ()
   {
-    if (paused_)
-      return;
-
     var state = create_empty_state ();
     var try_pos = { x: pos_.x + 1, y: pos_.y };
     if (place_shape (state_, state, current_shape (), try_pos))
     {
-      update_game (state_, state);
+      update_game_cb (state_, state);
       pos_.x += 1;
     }
   }
 
   function on_ctrl_down ()
   {
-    if (!paused_)
-      step ();
+    step ();
   }
 
   function on_ctrl_toggle ()
   {
-    if (!paused_)
-      toggle_orientation ();
+    toggle_orientation ();
   }
 
   function is_full_row (row)
@@ -444,21 +371,6 @@ var game0 = (function ($, document) {
     return { state: out, count: count };
   }
 
-  function get_score ()
-  {
-    return score_;
-  }
-
-  function get_lines ()
-  {
-    return lines_;
-  }
-
-  function set_paused (paused)
-  {
-    paused_ = paused;
-  }
-
   return {
     init: init,
     step: step,
@@ -467,8 +379,5 @@ var game0 = (function ($, document) {
     on_ctrl_toggle: on_ctrl_toggle,
     on_ctrl_down: on_ctrl_down,
     reset: reset,
-    get_score: get_score,
-    get_lines: get_lines,
-    set_paused: set_paused
   };
 } (jQuery, document));
